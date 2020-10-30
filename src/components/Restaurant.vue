5451a68a8190ba39a6a3e982d8b5c6215c7154cf
Dillon Harris <dillon.keith.harris96@gmail.com>
2020-10-30 13:22:39 -0600

    Adds style

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,44 +1,106 @@
 <template>
   <v-container class="mx-auto">
     <v-row>
-      <v-col>
-        <span>
-          Restaurant Name: {{restaurant.business_name || "restaurant.name"}}
-        </span>
-      </v-col>
-      <v-col>
-        <span>
-          Chef On Duty: {{chef.first_name + " " + chef.last_name || "chef.name"}}
-        </span>
-      </v-col>
-      <v-col>
-        <span>
-          Your Waiter: {{waiter.first_name + " " + waiter.last_name || "waiter.name"}}
-        </span>
-      </v-col>
+      <span class="mx-auto my-10 main-title">
+        {{restaurant.business_name || "restaurant.name"}}
+      </span>
+    </v-row>
+    <v-row class="mb-10">
+      <v-card
+        width="100%"
+      >
+        <v-container>
+          <v-row>
+            <v-col style="text-align: center">
+              <span class="mid-title">
+                Staff On Hand
+              </span>
+            </v-col>
+          </v-row>
+          <v-row style="max-width: 80%" class="mx-auto">
+            <v-col class="ml-10">
+              <v-row>
+                <v-col cols="4" class="ma-0">
+                  <!-- image -->
+                  <div cols="" class="dot ma-0">
+                    <v-avatar
+                      color="primary"
+                      size="106"
+                      style="float: right;"
+                    >
+                      <img
+                        src="./../assets/godonramsay.jpg"
+                        alt="Chef"
+                      >
+                    </v-avatar>
+                  </div>
+                  <span style="margin-left: 40px" class="little-text">Chef</span>
+                </v-col>
+                <v-col class="mt-10">
+                  <!-- name -->
+                  <span class="names">
+                    {{chef.first_name + " " + chef.last_name || "chef.name"}}
+                  </span>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col>
+              <v-row>
+                <v-col cols="4" class="ma-0">
+                  <div class="dot">
+                    <v-avatar
+                      color="primary"
+                      size="106"
+                      style="float: right;"
+                    >
+                      <img
+                        src="./../assets/waitress.jpg"
+                        alt="Waiter"
+                      >
+                    </v-avatar>
+                  </div>
+                  <span style="margin-left: 40px" class="little-text">Waiter</span>
+                </v-col>
+                <v-col class="mt-10">
+                  <!-- name -->
+                  <span class="names">
+                    {{waiter.first_name + " " + waiter.last_name || "waiter.name"}}
+                  </span>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-row>
+
+    <v-row>
+      <div style="width: 100%;">
+        <v-text-field
+          v-model="partyName"
+          label="Enter Party Name"
+          outlined
+        />
+      </div>
     </v-row>
     <v-row>
-      <v-text-field
-        v-model="partyName"
-        label="Enter Party Name"
-        outlined
-      />
+      <div style="width: 100%;">
+        <v-select
+          :items="[1,2,3,4,5,6,7,8,9,10]"
+          label="Party Size"
+          outlined
+          @input="setNumPeople($event)"
+        />
+      </div>
     </v-row>
-    <v-row>
-      <v-select
-        :items="[1,2,3,4,5,6,7,8,9,10]"
-        label="Number In Group"
-        outlined
-        @input="setNumPeople($event)"
-      />
-    </v-row>
+    
     <v-row
       v-for="person in numPeople"
       :key="person"
     >
       <v-select
         :items="menu"
-        label="Select Items to Order"
+        :label="'Guest '+person"
         outlined
         multiple
         item-text="item_name"
@@ -46,26 +108,49 @@
         @change="selectItem($event)"
       />
     </v-row>
-    <v-row>
-      <span>
-        Sub Total: {{'$' + getPriceSelectedItems().toFixed(2)}}
-      </span>
+    <v-row class="mx-auto my-10" style="max-width: 80%">
+      <v-col>
+        <!-- subTotal -->
+        <v-row class="mid-title">
+          {{'$' + getPriceSelectedItems().toFixed(2)}}
+        </v-row>
+        <v-row class="little-text">
+          Sub Total
+        </v-row>
+      </v-col>
+      <v-col style="text-align: center">
+        <span class="mid-title">+</span>
+      </v-col>
+      <v-col>
+        <!-- selecttip -->
+        <v-select
+          :items="tipAmounts"
+          label="Tip"
+          outlined
+          @input="setTipAmount($event)"
+        />
+      </v-col>
+      <v-col style="text-align: center">
+        <span class="mid-title">=</span>
+      </v-col>
+      <v-col>
+        <!-- grandtotal -->
+        <v-row>
+          <span style="font-weight: bold" class="mid-title">
+            {{'$' + getGrandTotal().toFixed(2)}}
+          </span>
+        </v-row>
+        <v-row>
+          <span class="little-text">
+            Grand Total
+          </span>
+        </v-row>
+      </v-col>
     </v-row>
-    <v-row>
-      <v-select
-        :items="tipAmounts"
-        label="Tip Amount"
-        outlined
-        @input="setTipAmount($event)"
-      />
-    </v-row>
-    <v-row>
-      <span>
-        Total: {{'$' + getGrandTotal().toFixed(2)}}
-      </span>
-    </v-row>
-    <v-row>
+    <v-row class="mb-10">
       <v-btn
+        color="#2BB4BD"
+        dark
         width="100%"
         elevation="2"
         x-large
@@ -74,7 +159,7 @@
         Pay
       </v-btn>
     </v-row>
-    <v-row v-if="paid">
+    <v-row class="mb-10" v-if="paid">
       <span>Payment Successful!</span>
     </v-row>
   </v-container>
@@ -194,5 +279,38 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.dot {
+  height: 120px;
+  width: 120px;
+  background-color: #2BB4BD;
+  border-radius: 50%;
+  /* display: inline-block; */
+}
+.main-title {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 56px;
+  text-align: center;
+}
+.mid-title {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+}
+.little-text {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
+}
+.names {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 28px;
+}
 </style>
